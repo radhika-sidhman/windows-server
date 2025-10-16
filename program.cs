85e79032db64537d645f9d694a25f1d7a92ecd50
radhika-sidhman <radhika.arane@sidhman.com>
2025-10-16 13:17:22 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -6,7 +6,7 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from Jenkins CI/CD Pipeline! [Webhook Test ok]");
+            Console.WriteLine("Hello from Jenkins CI/CD Pipeline! [Webhook Test ok done]");
             Console.WriteLine("Current Time: " + DateTime.Now);
         }
     }
